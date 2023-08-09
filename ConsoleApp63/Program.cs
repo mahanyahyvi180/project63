15971553649e793364ef1bccbc2fa0edac9d0367
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 17:39:24 +0330

    Add files via upload

---
+++ ConsoleApp63/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number :");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter a number :");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+            Console.ReadLine();
+        }
+    }
+}
